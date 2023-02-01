ddc2dc7cc16caa1df1504639a89e5617036e94f6
jiawei <jiaweizhou5@gmail.com>
2023-01-31 21:07:42 -0500

    update

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -1,5 +1,6 @@
 var metas = document.getElementsByTagName('meta');
 var i;
+
 if (navigator.userAgent.match(/iPhone/i)) {
   for (i=0; i<metas.length; i++) {
     if (metas[i].name == "viewport") {
@@ -8,10 +9,25 @@ if (navigator.userAgent.match(/iPhone/i)) {
   }
   document.addEventListener("gesturestart", gestureStart, false);
 }
+
 function gestureStart() {
   for (i=0; i<metas.length; i++) {
     if (metas[i].name == "viewport") {
       metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
     }
   }
+}
+
+var coll = document.getElementsByClassName("collapsible");
+var n;
+for (n = 0; n < coll.length; n++) {
+  coll[n].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
